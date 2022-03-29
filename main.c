438432c4a124745b45cdd7300c9c2912bd62975a
Han-g <ark9854@naver.com>
2022-03-30 03:49:04 +0900

    c
    sad

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
-#define Case 4
+#define Case 5
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <windows.h>
 #include <tchar.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
 
 struct RECT
 {
@@ -61,14 +64,17 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	PAINTSTRUCT ps;
 	HDC hDC;
 	TCHAR lpOut[100];
+	int lp[100];
 	memset(lpOut, NULL, sizeof(lpOut));
+	memset(lp, NULL, sizeof(lp));
 	RECT rect;
 	COLORREF text_color;
 	COLORREF BG_color;
-	int	x, y, n;
-	static int count;
+	int	x = 0, y = 0, n;
+	static int count = 0, c = 0;
 	static SIZE size;
 	static TCHAR str[100]; // = _T("abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVYXYZ abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVYXY Zabcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVYXYZ");
+	static int yPos = 0;
 
 	switch (uMsg) {
 	case WM_CREATE:
@@ -77,7 +83,16 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		count = 0;
 		break;
 	case WM_CHAR:
-		str[count++] = wParam;
+		if (wParam == VK_BACK) count--;
+		else if (wParam == VK_RETURN) {
+			count = 0;
+			yPos = yPos + 20;
+		}
+		else if (wParam == '0') exit(-1);
+		else str[count++] = wParam;
+
+		if (count >= 30) count = 0;
+
 		str[count] = '\0';
 		InvalidateRect(hWnd, NULL, TRUE);
 		break;
@@ -102,7 +117,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		wsprintf(lpOut, L"Center (%d, %d)", (rect.left+rect.right)/2, (rect.top+rect.bottom)/2);
 		TextOut(hDC, (rect.left + rect.right) / 2, (rect.top + rect.bottom) / 2, lpOut, lstrlen(lpOut));
 		}
-		 // 2-1
 
 #elif Case == 2
 		wsprintf(str, L"ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ");
@@ -146,7 +160,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		SetTextColor(hDC, text_color);
 		SetBkColor(hDC, BG_color);
 		DrawText(hDC, str, _tcsclen(str), &rect, DT_LEFT | DT_VCENTER | DT_WORDBREAK);
-		 // 2-2
+		
 #elif Case == 3
 		
 		x = rand() % 700; y = rand() % 500; count = rand() % 80 + 20; n = rand() % 9+1;
@@ -154,19 +168,39 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		BG_color = RGB(rand() % 255, rand() % 255, rand() % 255);
 		SetTextColor(hDC, text_color);
 		SetBkColor(hDC, BG_color);
-		int* tem = malloc(sizeof(int) * count);
-		for (int i = 0; i < count; i++)
-		{
-			tem[i] = n;	
-		}
-		strcpy_s(str, sizeof(tem), tem);
-		rect.left = x;
-		rect.top = y;
+		GetTextExtentPoint32(hDC, str, lstrlen(str), &size);
+		rect.left = x; rect.top = y;
+		char tem = NULL;
 		
-		DrawText(hDC, str, _tcsclen(str) - 1, &rect, DT_LEFT | DT_VCENTER | DT_WORDBREAK);
+		for (int i = 0; i < count; ++i) {
+			tem = n + '0';
+			str[i] = tem;
+		}
+
+		DrawText(hDC, str, _tcsclen(str) - 1, &rect, DT_CENTER | DT_VCENTER | DT_WORDBREAK);
+		EndPaint(hWnd, &ps);
+		break;
 #elif Case == 4
-		TextOut(hDC, 0, 0, str, lstrlen(str));
+		int table = 2, tem = 0;
+		static TCHAR TimeTable[19];
+		
+		for (int i = 0; i < 20; i++)
+		{
+			GetTextExtentPoint32(hDC, str, lstrlen(str), &size);
+			TimeTable[i] = table * i;
+			wsprintf(str, L"%d\t", TimeTable[i]);
+			TextOut(hDC, x, y, str, lstrlen(str));
+			x += size.cx;
+		}
+		size.cx = x;
 		SetCaretPos(size.cx, 0);
+
+		EndPaint(hWnd, &ps);
+		break;
+#elif Case == 5
+		TextOut(hDC, 0, 0, str, lstrlen(str));
+		
+		SetCaretPos(size.cx, y);
 
 		EndPaint(hWnd, &ps);
 		break;
